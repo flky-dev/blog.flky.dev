3542bcdec39f3c716c29fb01a33ff77e32540b60
Flonky <64008721+Flonky@users.noreply.github.com>
2021-12-30 15:56:21 +0100

    -> Optimize cookie.js -> Load cookie.js over head.html instead of extended_head.html -> Minify cookie.js in build -> Fingerprint cookie.js in build -> Update Cookie Banner CSS to update depending on theme

---
+++ themes/PaperMod/assets/js/cookies.js
@@ -0,0 +1,41 @@
+const cookieContainer = document.querySelector(".cookie-container");
+const cookieButton = document.querySelector(".cookie-btn");
+const nopeCookieButton = document.querySelector(".nope-cookie-btn");
+const cookieBannerDisplayed = "cookieBannerDisplayed"
+const analyticsIsAllowed = "analyticsIsAllowed"
+
+const analytics = (state) => {
+    const script = document.createElement("script");
+    script.async = true;
+    script.src = "https://analytics.flky.dev/tracker.js";
+    script.setAttribute("data-ackee-server", "https://analytics.flky.dev");
+    script.setAttribute("data-ackee-domain-id", "92cee36f-4b2a-4df4-87d9-7532b5692618");
+    script.setAttribute("data-ackee-opts", `{ \"ignoreLocalhost\": true, \"detailed\": ${state} }`);
+    document.head.appendChild(script);
+}
+
+
+if (localStorage.getItem(analyticsIsAllowed) === "true") {
+    analytics(state="true");
+}
+
+cookieButton.addEventListener("click", () => {
+    cookieContainer.classList.remove("active");
+    localStorage.setItem(cookieBannerDisplayed, "true");
+    localStorage.setItem(analyticsIsAllowed, "true");
+    analytics(state="true");
+});
+
+nopeCookieButton.addEventListener("click", () => {
+    cookieContainer.classList.remove("active");
+    localStorage.setItem(cookieBannerDisplayed, "true");
+    localStorage.setItem(analyticsIsAllowed, "false");
+    analytics(state="false");
+});
+
+setTimeout(() => {
+    if (!localStorage.getItem(cookieBannerDisplayed)) {
+        cookieContainer.classList.add("active");
+
+    }
+}, 100);
